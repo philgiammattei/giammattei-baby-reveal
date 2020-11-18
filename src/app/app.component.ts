814b47ad7fd66c6847716a4c294b8087c09e6a2a
Phil Giammattei <phillip.giammattei@clarivate.com>
2020-11-18 17:05:41 -0500

    boy test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'giammattei-baby-reveal';
-
   clicked = false;
+  countdown: number = null;
+  showVideo = false;
+
+  reveal(): any {
+    this.clicked = true;
+    this.countdown = 5;
+    const interval = setInterval(() => {
+      this.countdown--;
+      // Display 'counter' wherever you want to display it.
+      if (this.countdown === 0) {
+        this.countdown = null;
+        this.playVideo();
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  playVideo(): any {
+    this.showVideo = true;
+  }
 }
